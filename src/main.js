c00fc0009a6757418201724337d4339ce7d1c4ca
Fernando <fernandomadethat@gmail.com>
2024-11-14 16:17:39 -0300

    feat: adding effacts

--- src/main.js
+++ src/main.js
@@ -6,32 +6,6 @@ $('.bxslider').bxSlider({
   slideWidth: 600
 });
 
-function handleScroll() {
-  let scrollbar = Math.ceil(window.scrollY);
-
-  let sideBarBlur = [];
-
-  if(scrollbar == 0) {
-    sideBarBlur = []
-  }
-  else if(scrollbar > 420) {
-    sideBarBlur = ['bg-slate-800', 'shadow-zinc-800', 'shadow-sm']
-  }
-  else {
-
-    sideBarBlur = ['bg-slate-800/40', 'shadow-zinc-800', 'shadow-sm']
-  }
-
-  document.querySelector('#navbar').classList = []
-
-  let classList = [...sideBarBlur, 'w-full', 'flex', 'justify-end', 'items-center', 'py-2', 'fixed'];
-
-  classList.forEach(cl => {
-    document.querySelector('#navbar').classList.add(cl);
-  });
-}
-
-window.addEventListener('scroll', handleScroll)
 
 $('.access-solicitation').on('click', () => Swal.fire({
   title: "Solicitar Acesso",
@@ -49,7 +23,7 @@ $('.access-solicitation').on('click', () => Swal.fire({
     }
   }))
 
-window.onload = async () =>{
+async function writeTitle() {
   let title = document.querySelector('#title');
   let name = '< Fernando L. Silvestre Fullstack />';
 
@@ -90,3 +64,91 @@ function cursor() {
     }
   };
 }
+
+
+const staggerVisualizerEl = document.querySelector('.stagger-visualizer');
+const fragment = document.createDocumentFragment();
+const grid = [17, 17];
+const col = grid[0];
+const row = grid[1];
+const numberOfElements = col * row;
+
+for (let i = 0; i < numberOfElements; i++) {
+  fragment.appendChild(document.createElement('div'));
+}
+
+staggerVisualizerEl.appendChild(fragment);
+
+const staggersAnimation = anime.timeline({
+  targets: '.stagger-visualizer div',
+  easing: 'easeInOutSine',
+  delay: anime.stagger(50),
+  loop: true,
+  autoplay: false
+})
+.add({
+  translateX: [
+    {value: anime.stagger('-.1rem', {grid: grid, from: 'center', axis: 'x'}) },
+    {value: anime.stagger('.1rem', {grid: grid, from: 'center', axis: 'x'}) }
+  ],
+  translateY: [
+    {value: anime.stagger('-.1rem', {grid: grid, from: 'center', axis: 'y'}) },
+    {value: anime.stagger('.1rem', {grid: grid, from: 'center', axis: 'y'}) }
+  ],
+  duration: 1000,
+  scale: .5,
+  delay: anime.stagger(100, {grid: grid, from: 'center'})
+})
+.add({
+  translateX: () => anime.random(-10, 10),
+  translateY: () => anime.random(-10, 10),
+  delay: anime.stagger(8, {from: 'last'})
+})
+.add({
+  translateX: anime.stagger('.25rem', {grid: grid, from: 'center', axis: 'x'}),
+  translateY: anime.stagger('.25rem', {grid: grid, from: 'center', axis: 'y'}),
+  rotate: 0,
+  scaleX: 2.5,
+  scaleY: .25,
+  delay: anime.stagger(4, {from: 'center'})
+})
+.add({
+  rotate: anime.stagger([90, 0], {grid: grid, from: 'center'}),
+  delay: anime.stagger(50, {grid: grid, from: 'center'})
+})
+.add({
+  translateX: 0,
+  translateY: 0,
+  scale: .5,
+  scaleX: 1,
+  rotate: 180,
+  duration: 1000,
+  delay: anime.stagger(100, {grid: grid, from: 'center'})
+})
+.add({
+  scaleY: 1,
+  scale: 1,
+  delay: anime.stagger(20, {grid: grid, from: 'center'})
+})
+
+staggersAnimation.play();
+
+
+setTimeout(()=> {
+$('.overlay').css({
+  background: 'rgba(0,0,0,0)'
+})
+}, 2000);
+
+setTimeout(()=>{
+  $('.overlay').hide(500);
+  $('#description').toggleClass('opacity-0', 'opacity-1')
+  $('#header').removeClass('pb-80')
+  document.querySelectorAll('img').forEach(img => {
+    if($(img).hasClass('opacity-0')) {
+      $(img).toggleClass('opacity-0', 'opacity-1')
+    }
+  });
+  
+  writeTitle();
+},4000);
